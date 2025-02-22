a5321c7c3e4520a93bfb84312335437b560d969a
ohno <akifumi139@gmail.com>
2025-02-22 11:31:07 +0000

    chore: tailwindの導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,11 @@ export default {
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
-        './resources/**/*.vue',
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: [...defaultTheme.fontFamily.sans],
             },
         },
     },
