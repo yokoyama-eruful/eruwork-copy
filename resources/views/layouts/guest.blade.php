060066151a0ad16bff23c8b5ca8a7726120503f0
yokoyama <yokoyama@eruful.jp>
2025-02-27 00:43:34 +0000

    feat: 不要なフォントリンクを削除し、コードをクリーンアップ

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,11 +8,6 @@
 
   <title>{{ config('app.name', 'Laravel') }}</title>
 
-  <!-- Fonts -->
-  <link href="https://fonts.bunny.net" rel="preconnect">
-  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-  <!-- Scripts -->
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
