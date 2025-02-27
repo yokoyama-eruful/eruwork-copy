39f87934efef68d6175ac7f5cfae9652ea6a9d23
yokoyama <yokoyama@eruful.jp>
2025-02-27 04:44:06 +0000

    feat: TenancyServiceProviderを追加し、厳密な型宣言を有効化

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+    App\Providers\TenancyServiceProvider::class,
 ];
