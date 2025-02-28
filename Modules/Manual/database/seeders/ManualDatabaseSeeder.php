9d24e2d92cc2a6785878737de62512e8f91a37bd
yokoyama <yokoyama@eruful.jp>
2025-02-28 09:14:01 +0000

    feat: Manualモジュールを追加し、基本的な構成ファイルとルーティングを設定

---
+++ Modules/Manual/database/seeders/ManualDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Manual\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ManualDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
