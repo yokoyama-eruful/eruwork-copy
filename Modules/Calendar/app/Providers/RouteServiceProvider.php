b69c34911efd2785175f2236128ccd4cb55663c8
yokoyama <yokoyama@eruful.jp>
2025-03-03 00:15:26 +0000

    feat: カレンダーモジュールの初期設定と基本構造を追加

---
+++ Modules/Calendar/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Calendar\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    protected string $name = 'Calendar';
+
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     */
+    public function map(): void
+    {
+        $this->mapWebRoutes();
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     */
+    protected function mapWebRoutes(): void
+    {
+        Route::middleware('web')->group(module_path($this->name, '/routes/web.php'));
+    }
+}
--- resources/views/home/layouts/sidemenu.blade.php
+++ resources/views/home/layouts/sidemenu.blade.php
@@ -19,8 +19,7 @@
       </li>
       <li class="mb-2"><a class="block h-20 w-20 rounded border bg-sky-100 hover:bg-gray-100" href="#">掲示板</a>
       </li>
-      <li class="mb-2"><a class="block h-20 w-20 rounded border bg-sky-100 hover:bg-gray-100"
-          href="#">タイムカード</a></li>
+      <li class="mb-2">@include('calendar::icon')</li>
       <li class="mb-2"><a class="block h-20 w-20 rounded border bg-sky-100 hover:bg-gray-100"
           href="#">タイムカード</a></li>
       <li class="mb-2"><a class="block h-20 w-20 rounded border bg-sky-100 hover:bg-gray-100"
