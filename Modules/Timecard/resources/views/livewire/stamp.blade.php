42ea40180c402965192c4158f44d5f990918a2b7
yokoyama <yokoyama@eruful.jp>
2025-03-19 02:34:47 +0000

    feat: タイムカードのボタンレイアウトを改善し、レスポンシブデザインを強化

--- Modules/Timecard/resources/views/livewire/stamp.blade.php
+++ Modules/Timecard/resources/views/livewire/stamp.blade.php
@@ -2,19 +2,19 @@
   <div class="flex space-x-3 pb-2">
     <div class="text-lg font-bold">タイムカード</div>
   </div>
-  <div class="my-5 flex w-full flex-col items-center justify-center space-x-5 sm:flex-row">
+  <div class="my-5 flex w-full flex-col items-center justify-center space-y-5 sm:flex-row sm:space-x-5 sm:space-y-0">
     <div class="my-2 flex w-full flex-col items-center sm:w-3/5">
-      <div class="mb-5 flex flex-row items-center space-x-3 font-extrabold" wire:poll.20s="updateClock">
+      <div class="mb-5 flex flex-row items-center font-extrabold sm:space-x-3" wire:poll.20s="updateClock">
         <p class="text-xl">{{ $currentDate }}</p>
         <p class="text-4xl">{{ $currentTime }}</p>
       </div>
 
-      <div class="flex w-full flex-row justify-center space-x-7">
-        <button class="shape-stamp-in relative h-16 w-1/2 text-xl font-extrabold" wire:click="push('in')"
+      <div class="flex w-full flex-row justify-center space-x-5">
+        <button class="shape-stamp-in relative h-16 w-full text-xl font-extrabold sm:w-1/2" wire:click="push('in')"
           @if (array_search('in', $buttonStatus) === false) disabled @endif>
           出　勤
         </button>
-        <button class="shape-stamp-out tablet:block relative h-16 w-1/2 text-xl font-extrabold" wire:click="push('out')"
+        <button class="shape-stamp-out relative h-16 w-full text-xl font-extrabold sm:w-1/2" wire:click="push('out')"
           @if (array_search('out', $buttonStatus) === false) disabled @endif>
           退　勤
         </button>
