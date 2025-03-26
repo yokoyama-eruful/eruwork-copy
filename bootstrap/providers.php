683cba22646d2db789f1318f90aaeeeacc884a48
yokoyama <yokoyama@eruful.jp>
2025-03-26 15:32:08 +0900

    feat: WebPushNotificationクラスをfinalに変更し、WebPushサービスプロバイダーの登録を削除

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 return [
     App\Providers\AppServiceProvider::class,
-    NotificationChannels\WebPush\WebPushServiceProvider::class,
-    App\Providers\BroadcastServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     App\Providers\TenancyServiceProvider::class,
 ];
