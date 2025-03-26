8ff59c39db263df2f3698ab110d38d78dc3573a0
yokoyama <yokoyama@eruful.jp>
2025-03-26 15:29:40 +0900

    feat: WebPushサービスプロバイダーを追加し、PWAメタタグの配置を更新

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     App\Providers\AppServiceProvider::class,
+    NotificationChannels\WebPush\WebPushServiceProvider::class,
     App\Providers\BroadcastServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     App\Providers\TenancyServiceProvider::class,
