aa04804c95037cc607fc73f1f35d8d7a4fab3ebe
yokoyama <yokoyama@eruful.jp>
2025-03-27 16:16:06 +0900

    feat: シフト　確定シフト時刻と希望シフト時刻の確認が行えるように修正

--- Modules/Shift/resources/views/general/livewire/submission-edit-modal.blade.php
+++ Modules/Shift/resources/views/general/livewire/submission-edit-modal.blade.php
@@ -2,28 +2,39 @@
   @if ($schedule->shiftStatus || !$manager->OverSubmissionPeriod)
     <div class="px-4 pb-4">
       <div class="mt-4 text-start text-lg font-bold">
-        {{ $form->date }}
+        {{ $schedule->date->format('Y年m月d日') }}
       </div>
+
       <div class="mt-4">
-        <x-input-label class="text-start" for="start_time" value="開始時間" />
+        <x-input-label class="text-start" value="希望開始時間" />
 
-        <x-text-input class="mt-1 block w-full" id="start_time" name="start_time" type="time" readonly
-          wire:model="form.startTime" required />
-
-        @error('form.start_time')
-          <div class="font-normal text-red-500">{{ $message }}</div>
-        @enderror
+        <div class="border-b p-2 text-left">
+          {{ $schedule->start_time->format('H:i') }}
+        </div>
       </div>
 
-      <div class="mt-2">
-        <x-input-label class="text-start" for="end_time" value="終了時間" />
+      <div class="mt-4">
+        <x-input-label class="text-start" value="希望終了時間" />
 
-        <x-text-input class="mt-1 block w-full" id="end_time" name="end_time" type="time" readonly
-          wire:model="form.endTime" required />
+        <div class="border-b p-2 text-left">
+          {{ $schedule->end_time->format('H:i') }}
+        </div>
+      </div>
 
-        @error('form.endTime')
-          <div class="font-normal text-red-500">{{ $message }}</div>
-        @enderror
+      <div class="mt-4">
+        <x-input-label class="text-start" value="確定開始時間" />
+
+        <div class="border-b p-2 text-left">
+          {{ $schedule->shiftSchedule->start_time->format('H:i') }}
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <x-input-label class="text-start" value="確定終了時間" />
+
+        <div class="border-b p-2 text-left">
+          {{ $schedule->shiftSchedule->end_time->format('H:i') }}
+        </div>
       </div>
 
       <div class="mt-6 flex justify-end">
@@ -42,7 +53,7 @@
       @csrf
 
       <div class="mt-4 text-start text-lg font-bold">
-        {{ $form->date }}
+        {{ $schedule->date->format('Y年m月d日') }}
       </div>
       <div class="mt-4">
         <x-input-label class="text-start" for="start_time" value="開始時間" />
--- resources/views/components/widget.blade.php
+++ resources/views/components/widget.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full xl:mb-4">
+<div class="w-full border-y xl:mb-4 xl:border-none">
   <div class="overflow-hidden bg-white shadow-sm xl:rounded-lg">
     <div class="p-4 text-gray-900">
       {{ $slot }}
