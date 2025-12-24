05db851a24f2275df9c0442276f8eea4b5c652ad
yokoyama <yokoyama@eruful.jp>
2025-12-24 16:50:01 +0900

    fix

--- resources/views/timecard/login.blade.php
+++ resources/views/timecard/login.blade.php
@@ -48,19 +48,7 @@
     </div>
   </div>
   <script>
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.getRegistrations().then(registrations => {
-        registrations.forEach(registration => {
-          registration.unregister();
-        });
-      });
-    }
-
-    if ('caches' in window) {
-      caches.keys().then(keys => {
-        keys.forEach(key => caches.delete(key));
-      });
-    }
+    window.Notification = undefined;
   </script>
 </body>
 
