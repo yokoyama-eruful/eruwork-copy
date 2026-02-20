efdf11abdada6df7bbeec115ccec4a35c429e11b
furumo1029 <furumoto@eruful.jp>
2026-02-20 18:09:20 +0900

    チャットのリスト修正

--- Modules/Chat/resources/views/general/livewire/member-list.blade.php
+++ Modules/Chat/resources/views/general/livewire/member-list.blade.php
@@ -2,13 +2,14 @@
   <div class="mb-6 text-[11px] font-bold lg:mb-[7px]">グループ一覧</div>
   @foreach ($this->groups->where('is_dm', false) as $group)
     <a href="{{ route('chat.show', ['group' => $group]) }}" @class([
-        'grid grid-cols-[30px,auto,30px] lg:py-2 py-[20px] rounded gap-1 my-1 lg:border-none border-b lg:-mx-0 -mx-[15px] lg:px-0 px-[20px]',
+        'grid grid-cols-[30px,auto,30px] items-center lg:py-2 py-[20px] rounded gap-1 my-1 lg:border-none border-b lg:-mx-0 -mx-[15px] lg:px-0 px-[20px]',
         'bg-[#3289FA1A] bg-opacity-10 border border-[#3289FA]' =>
             $selectGroup?->id == $group?->id,
     ]) @click="mobileTalkArea=true">
       <div class="flex items-center justify-center">
         @if ($group->IconImage)
-          <img class="h-[25px] w-[25px] rounded-full border bg-white" src="{{ $group->IconImage }}" alt="アイコン">
+          <img class="h-[30px] w-[30px] rounded-full border bg-white lg:h-[25px] lg:w-[25px]"
+            src="{{ $group->IconImage }}" alt="アイコン">
         @else
           <div class="flex h-[25px] w-[25px] items-center justify-center rounded-full border bg-white"><i
               class="fa-solid fa-image text-[25px]"></i>
@@ -31,13 +32,13 @@
   @foreach ($this->groups->where('is_dm', true) as $group)
     @if ($group->partnerUser !== null)
       <a href="{{ route('chat.show', ['group' => $group]) }}" @class([
-          'grid grid-cols-[30px,auto,30px] lg:py-2 py-[20px] rounded gap-1 my-1 lg:border-none border-b lg:-mx-0 -mx-[15px] lg:px-0 px-[20px]',
+          'grid grid-cols-[30px,auto,30px] items-center lg:py-2 py-[20px] rounded gap-1 my-1 lg:border-none border-b lg:-mx-0 -mx-[15px] lg:px-0 px-[20px]',
           'bg-[#3289FA1A] bg-opacity-10 border border-[#3289FA]' =>
               $selectGroup?->id == $group?->id,
       ]) @click="mobileTalkArea=true">
         <div class="flex items-center justify-center">
           @if ($group->IconImage)
-            <img class="h-[25px] w-[25px] rounded-full border bg-white"
+            <img class="h-[30px] w-[30px] rounded-full border bg-white lg:h-[25px] lg:w-[25px]"
               src="{{ route('profile.icon', ['id' => $group->partnerUser->id]) }}" alt="アイコン">
           @else
             <div class="flex h-[25px] w-[25px] items-center justify-center rounded-full border bg-white"><i
@@ -45,7 +46,7 @@
             </div>
           @endif
         </div>
-        <div class="ml-[10px] truncate text-[15px] font-semibold">{{ $group->NameLabel }}</div>
+        <div class="ml-[10px] flex items-center truncate text-[15px] font-semibold">{{ $group->NameLabel }}</div>
         <div class="flex items-center justify-center">
           @if ($group->group_notification_count)
             <div
