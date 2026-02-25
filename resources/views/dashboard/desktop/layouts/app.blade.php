81c9732ce7f3a9259a7ce66f456a5bb753edd106
furumo1029 <furumoto@eruful.jp>
2026-02-25 13:58:52 +0900

    モバイル拡大対応

--- resources/views/dashboard/desktop/layouts/app.blade.php
+++ resources/views/dashboard/desktop/layouts/app.blade.php
@@ -38,10 +38,10 @@
       <div class="flex flex-col items-center bg-[#F7F7F7] px-5 pb-8 pt-4 text-left">
         <div class="pt-[13px] text-[0.9375rem] font-bold">ログアウトしますか</div>
       </div>
-      <div class="my-5 flex items-center justify-center space-x-[10px]">
-        <div class="flex h-11 w-[150px] cursor-pointer items-center justify-center rounded border-2"
+      <div class="my-5 flex flex-col gap-[10px] sm:flex-row sm:items-center sm:justify-center sm:space-x-[10px] sm:gap-0">
+        <div class="flex h-11 w-full sm:w-[150px] cursor-pointer items-center justify-center rounded border-2"
           @click="$dispatch('close-modal', 'logout')">キャンセル</div>
-        <button class="flex h-11 w-[150px] items-center justify-center rounded bg-[#FF4A62] text-white" type="submit">
+        <button class="flex h-11 w-full sm:w-[150px] items-center justify-center rounded bg-[#FF4A62] text-white" type="submit">
           ログアウト
         </button>
 
